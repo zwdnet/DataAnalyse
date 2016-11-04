7f1a888f678e76f8512edc53dc8b2f66d667108c
zym <zwdnet@163.com>
2016-11-04 15:58:05 +0800

    add the setting of IDE and python

--- dataAnalyse.py
+++ dataAnalyse.py
@@ -1,18 +1,18 @@
 # -*- coding:utf-8 -*-
-#读入csv文件，进行数据分析
+# 读入csv文件，进行数据分析
 import pandas as pd
 
-df = pd.read_csv('money.csv', encoding = 'gbk')
-#print(df[:3])
+df = pd.read_csv('money.csv', encoding='gbk')
+# print(df[:3])
 
-#数据处理
+# 数据处理
 import matplotlib.pyplot as plt
 # plt.plot(df['Amount'])
 # plt.show()
 
-#收入
+# 收入
 Income = []
-#支出
+# 支出
 Outcome = []
 for i in df["Amount"]:
     if i < 0:
@@ -28,7 +28,7 @@ plt.plot(Income)
 plt.plot(Outcome)
 plt.show()
 
-#计算累积收入支出
+# 计算累积收入支出
 accumuIncome = []
 accumuOutcome = []
 i = 0
@@ -36,16 +36,16 @@ for data in Income:
     if i == 0:
         accumuIncome.append(data)
     else:
-        accumuIncome.append(data + accumuIncome[i-1])
+        accumuIncome.append(data + accumuIncome[i - 1])
     i += 1
-#收入与支出的差额
+# 收入与支出的差额
 sub = []
 i = 0
 for data in Outcome:
     if i == 0:
         accumuOutcome.append(data)
     else:
-        accumuOutcome.append(data + accumuOutcome[i-1])
+        accumuOutcome.append(data + accumuOutcome[i - 1])
     sub.append(accumuIncome[i] - accumuOutcome[i])
     i += 1
 plt.plot(accumuIncome)
@@ -53,7 +53,7 @@ plt.plot(accumuOutcome)
 plt.plot(sub)
 plt.show()
 
-#将数据按类型分组
+# 将数据按类型分组
 grouped = df['Amount'].groupby(df['TypeName'])
 result = abs(grouped.sum())
 num = []
@@ -67,43 +67,63 @@ for d in result:
 plt.plot(num)
 plt.show()
 
-#print(df.dtypes)
+# print(df.dtypes)
 
-#按月分组
+# 按月分组
 i = 0
 Time = []
 for time in df['Time']:
-    #df['time'][0].values = int(time)
-    #print(int(time))
-    Time.append(int(time/100))
+    # df['time'][0].values = int(time)
+    # print(int(time))
+    Time.append(int(time / 100))
     i += 1
 
-#将原始数据转换为时间按月的数据
+# 将原始数据转换为时间按月的数据
 df_month = df
 df_month['Time'] = Time
-#print(df_month[:3])
-#按月份分组，计算每个月的纯收入
+# print(df_month[:3])
+# 按月份分组，计算每个月的纯收入
 grouped = df_month['Amount'].groupby(df_month['Time'])
 result = grouped.sum()
-#print(result)
+# print(result)
 num = []
 for d in result:
     num.append(d)
 plt.plot(num)
 plt.show()
 
-#按年分组
+# 分离出收入和支出，收入>0，支出<0
+df_month_income = df_month[df_month['Amount'] > 0]
+df_month_outcome = df_month[df_month['Amount'] < 0]
+incomeGroup = df_month_income.groupby(df_month_income['Time'])
+outcomeGroup = df_month_outcome.groupby(df_month_outcome['Time'])
+incomeSum = incomeGroup.sum()
+outcomeSum = outcomeGroup.sum()
+
+num = []
+for d in incomeSum['Amount']:
+    num.append(d)
+plt.plot(num)
+
+num = []
+for d in outcomeSum['Amount']:
+    num.append(abs(d))
+plt.plot(num)
+
+plt.show()
+
+# 按年分组
 i = 0
 Time = []
 for time in df_month['Time']:
-    Time.append(int(time/100))
+    Time.append(int(time / 100))
     i += 1
 
-#将原始数据转换为时间按年的数据
+# 将原始数据转换为时间按年的数据
 df_year = df
 df_year['Time'] = Time
-#print(df_year[:3])
-#按月份分组，计算每年的纯收入
+# print(df_year[:3])
+# 按月份分组，计算每年的纯收入
 grouped = df_year['Amount'].groupby(df_year['Time'])
 result = grouped.sum()
 # print(result)
@@ -113,7 +133,7 @@ for d in result:
 plt.plot(num)
 plt.show()
 
-#分离出收入和支出，收入>0，支出<0
+# 分离出收入和支出，收入>0，支出<0
 df_year_income = df_year[df_year['Amount'] > 0]
 df_year_outcome = df_year[df_year['Amount'] < 0]
 incomeGroup = df_year_income.groupby(df_year_income['Time'])
---
+++ font/font.bat
@@ -0,0 +1,2 @@
+copy .\*.ttf C:\Anaconda3\Lib\site-packages\matplotlib\mpl-data\fonts\ttf
+copy .\matplotlibrc C:\Anaconda3\Lib\site-packages\matplotlib\mpl-data
